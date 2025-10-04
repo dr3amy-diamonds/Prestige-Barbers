821d3a157ad9bc6cec4743f1496431dce0b4da23
dr3amy-diamond <vale05ojeda@gmail.com>
2025-10-04 10:31:47 -0500

    Base de datos ya conectada

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo");
+// 💡 LÍNEA CORREGIDA: Requiere el módulo 'mysql'
+const mysql = require('mysql');
+
+// MySQL conexión
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Barberia'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log("🟢 Conectado a MySQL");
+});
+
